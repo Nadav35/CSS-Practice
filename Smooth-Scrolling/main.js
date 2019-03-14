2f6540278fd944840d505394f01f6c6947ad38ce
Nadav <nadavnoy@yahoo.com>
2019-03-14 13:59:03 -0700

    smooth scrolling with smoothscrolling script

--- Smooth-Scrolling/main.js
+++ Smooth-Scrolling/main.js
@@ -1,17 +1,25 @@
 // Option 2 - jQuery smooth scroll
 
-$('.navbar a').on('click',  function(e) {
-  // eslint-disable-next-line no-invalid-this
-  if (this.hash !== '') {
-    e.preventDefault();
+// $('.navbar a').on('click',  function(e) {
+//   // eslint-disable-next-line no-invalid-this
+//   if (this.hash !== '') {
+//     e.preventDefault();
 
-    const hash = this.hash;
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      800
-    );
-  }
+//     const hash = this.hash;
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       800
+//     );
+//   }
   
-});
+// });
+
+// Option 3, smoothscroll script from GitHub
+
+// eslint-disable-next-line no-undef
+
+// const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+//   speed: 800
+// });
